6abb94fa832bbba723d83221b2868705e6f1729b
vthomas <me@valentin-thomas.com>
2022-01-28 20:23:10 +0100

    Fix isAlpha

--- tests/src/ts_isalpha.c
+++ tests/src/ts_isalpha.c
@@ -14,7 +14,11 @@ static void test_isalpha_c()
 static void test_cmp_isalpha()
 {
   check_cond(ft_isalpha('a') == isalpha('a'), "ft_isalpha('a') == isalpha('a')");
+  check_cond(ft_isalpha('g') == isalpha('g'), "ft_isalpha('g') == isalpha('g')");
+  check_cond(ft_isalpha('z') == isalpha('z'), "ft_isalpha('z') == isalpha('z')");
   check_cond(ft_isalpha('A') == isalpha('A'), "ft_isalpha('A') == isalpha('A')");
+  check_cond(ft_isalpha('G') == isalpha('G'), "ft_isalpha('G') == isalpha('G')");
+  check_cond(ft_isalpha('Z') == isalpha('Z'), "ft_isalpha('Z') == isalpha('Z')");
   check_cond(ft_isalpha('1') == isalpha('1'), "ft_isalpha('1') == isalpha('1')");
   check_cond(ft_isalpha('\0') == isalpha('\0'), "ft_isalpha('\\0') == isalpha('\\0')");
   check_cond(ft_isalpha('\n') == isalpha('\n'), "ft_isalpha('\\n') == isalpha('\\n')");
